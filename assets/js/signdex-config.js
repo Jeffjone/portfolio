bfc97ce5da2a125bdde13f39d90fd6c8156c9052
Jeff Jone <joshj.jeffrey@gmail.com>
2026-09-22 15:22:08 -0500

    Connect SignDex production API

--- assets/js/signdex-config.js
+++ assets/js/signdex-config.js
@@ -1,3 +1,5 @@
 // Set this to your deployed SignDex Worker URL. No API keys or secrets belong here.
 // Empty = honest preview mode: visitors can design cards, but nothing is submitted.
-globalThis.SignDexConfig = { apiBase: '' };
+globalThis.SignDexConfig = {
+  apiBase: 'https://jeffrey-signdex.joshj-jeffrey.workers.dev'
+};
